ce109a2595bf72bbe0c4b6b60323a90eef02ba22
coopyey <coopyey@coopyey.gay>
2024-10-18 09:45:48 -0500

    mapping update

--- keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -39,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_1    , KC_2     , KC_3     , KC_4     , KC_5         ,                                     KC_6         , KC_7    , KC_8     , KC_9     , KC_0     , KC_MINS  ,
     KC_GRV   , KC_Q    , KC_W     , KC_E     , KC_R     , KC_T         ,                                     KC_Y         , KC_U    , KC_I     , KC_O     , KC_P     , KC_BSLS  ,
     KC_CAPS  , KC_A    , KC_S     , KC_D     , KC_F     , KC_G         ,                                     KC_H         , KC_J    , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT  , KC_Z    , KC_X     , KC_C     , KC_V     , KC_B         , KC_RBRC      ,        KC_NUHS     , KC_N         , KC_M    , KC_COMM  , KC_DOT   , KC_SLSH  , KC_DEL   ,
-    KC_LCTL  , LCA(KC_DEL)   , KC_LWIN  , KC_LALT  , QK_KB_7  , LT(2,KC_SPC) , LT(3,KC_TAB) ,        LT(4,KC_ENT), LT(5,KC_BSPC),                                 TG(1)    , KC_INS
+    KC_LSFT  , KC_Z    , KC_X     , KC_C     , KC_V     , KC_B         , KC_LBRC      ,        KC_RBRC     , KC_N         , KC_M    , KC_COMM  , KC_DOT   , KC_SLSH  , KC_DEL   ,
+    KC_LCTL  , LCA(KC_DEL) , KC_LWIN , KC_LALT , LT(2,KC_SPC), LT(3,KC_TAB) , QK_KB_7 ,        LT(4,KC_ENT), LT(5,KC_BSPC),                                 TG(1)    , KC_INS
   ),
 
   // MOUSE
@@ -48,16 +48,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                                   KC_F6     , KC_F7      , KC_F8      , KC_F9      , KC_F10     , KC_F11   ,
     XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                                   C(KC_A)   , C(KC_C)    , C(KC_V)    , C(KC_X)    , C(KC_Z)    , KC_F12   ,
     XXXXXXX  , KC_LGUI , KC_LALT , KC_LCTL , KC_LSFT , TG(1)   ,                                   KC_BTN3   , KC_BTN1    , KC_BTN2    , XXXXXXX    , QK_KB_7    , QK_KB_13 ,
-    XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,               QK_KB_10, QK_KB_6   , KC_HOME    , KC_PGDN    , KC_PGUP    , KC_END     , QK_KB_15 ,
+    XXXXXXX  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,               XXXXXXX , QK_KB_6   , KC_HOME    , KC_PGDN    , KC_PGUP    , KC_END     , QK_KB_15 ,
     _______  , _______ , _______ , _______ , _______ , _______ , _______ ,               _______ , _______   ,                                        _______    , _______
   ),
 
   // L NAVIGATION & SCREENSHOT, R 10 KEY
   [2] = LAYOUT(
-    _______  , _______    , _______  , _______  , _______  , _______  ,                                     _______  , KC_P7    , KC_P8    , KC_P9    , KC_PSLS  , _______  ,
+    _______  , _______    , _______  , _______  , _______  , _______  ,                                     KC_PEQL  , KC_P7    , KC_P8    , KC_P9    , KC_PSLS  , _______  ,
     INSPE    , XXXXXXX    , XXXXXXX  , KC_UP    , XXXXXXX  , XXXXXXX  ,                                     KC_PAST  , KC_P4    , KC_P5    , KC_P6    , KC_PPLS  , _______  ,
     INSPP    , LSG(KC_S)  , KC_LEFT  , KC_DOWN  , KC_RIGHT , XXXXXXX  ,                                     KC_PCMM  , KC_P1    , KC_P2    , KC_P3    , KC_PMNS  , _______  ,
-    PFX      , XXXXXXX    , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , A(KC_F4)    ,            _______  , KC_PEQL  , KC_P0    , KC_P0    , KC_DOT   , KC_PENT  , _______  ,
+    PFX      , XXXXXXX    , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , A(KC_F4)    ,            _______  , KC_P0    , KC_P0    , XXXXXXX  , KC_DOT   , KC_PENT  , _______  ,
     _______  , _______    , _______  , _______  , _______  , _______  , _______     ,            _______  , _______  ,                                  _______  , _______
   ),
 
